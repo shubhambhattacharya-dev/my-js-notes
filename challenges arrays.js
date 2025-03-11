b07c0766ce623468770682ecb159b535a0923706
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-03-11 08:02:35 +0530

    practices

---
+++ challenges arrays.js
@@ -0,0 +1,16 @@
+ //hunt practice - is there any way or keyvoard shortcut in the VS code where i can actually have these multiline comments  being added or removed in one go?
+ //_________________________________________________________________________
+ 
+ const teaFlavour = ["green tea", "black tea", "oolong tea"];
+
+ let firstTea = console.log(teaFlavour[0]);
+ //_________________________________________________________________________
+ 
+ // another way :-
+
+//  const flvour= new array("green tea", "black tea", "oolong tea");
+// _________________________________________________________________________
+
+const cities =["London", "Tokoyo","Paris","New York"];
+let favourCiti= console.log(cities[2]);
+//_________________________________________________________________________
