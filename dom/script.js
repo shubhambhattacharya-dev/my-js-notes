318bf34a3e724612bf40f43b96092cbfc86487e5
shubhambhattacharya-dev <get.input.2025@gmail.com>
2025-04-11 12:17:53 +0530

    basic dom

---
+++ dom/script.js
@@ -0,0 +1,10 @@
+let newName = document.getElementById("me");
+
+console.dir(newName);
+
+let shu=document.getElementsByClassName("now");
+
+console.dir(shu);
+
+let newq=document.getElementsByTagName("h1");
+console.log(newq);
